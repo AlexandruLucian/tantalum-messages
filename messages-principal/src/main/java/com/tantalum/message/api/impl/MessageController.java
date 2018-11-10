64ead11ccf350d39e2becc952713dc5d77d078f3
AlexandruLucian <alexandru.lucian89@gamil.com>
2018-11-10 13:04:01 +0000

    Created two rest methods.

--- messages-principal/src/main/java/com/tantalum/message/api/impl/MessageController.java
+++ messages-principal/src/main/java/com/tantalum/message/api/impl/MessageController.java
@@ -1,8 +1,26 @@
 package com.tantalum.message.api.impl;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tantalum.messages.beans.impl.Message;
 
 @RestController
 public class MessageController {
 	
+	@RequestMapping(value = "/create", method = RequestMethod.POST, consumes = "application/json")
+	public ResponseEntity<Message> createMessage(@RequestBody Message message) {
+		
+		return new ResponseEntity<Message>(message,  HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value = "/get", method = RequestMethod.GET, consumes = "application/json")
+	public ResponseEntity<Message> getMessage(@RequestBody Message message) {
+		
+		return new ResponseEntity<Message>(message,  HttpStatus.CREATED);
+	}
 }
