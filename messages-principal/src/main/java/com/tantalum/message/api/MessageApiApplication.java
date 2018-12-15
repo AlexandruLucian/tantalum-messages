0042c6b6e428afab26bab2aecdc4486821cdf059
AlexandruLucian <alexandru.lucian89@gamil.com>
2018-12-15 16:48:59 +0000

    Created message UUID microservice

--- messages-principal/src/main/java/com/tantalum/message/api/MessageApiApplication.java
+++ messages-principal/src/main/java/com/tantalum/message/api/MessageApiApplication.java
@@ -6,16 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import com.tantalum.message.api.impl.MessageController;
-
 @SpringBootApplication(scanBasePackages={"com.tantalum.message"})
 @EnableDiscoveryClient
 public class MessageApiApplication {
 	
-	private static final Logger log = LoggerFactory.getLogger(MessageController.class);
+	private static final Logger log = LoggerFactory.getLogger(MessageApiApplication.class);
 
 	public static void main(String[] args) {
-		log.debug("====================");
 		SpringApplication.run(MessageApiApplication.class, args);
 	}
 }
